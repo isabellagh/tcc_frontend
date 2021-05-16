b9b632e6474de1eac69e8bd5e647ffe13d1a7bb1
isabellagh <isabellagh88@gmail.com>
2021-05-16 17:18:50 -0400

    getchildren function

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+const classroomsURL = "http://localhost:3000/api/v1/classrooms"
 const childrenURL = "http://localhost:3000/api/v1/children"
 
 document.addEventListener('DOMContentLoaded', () => {
-//   fetch(childrenURL)
-//   .then(response => response.json()) // capture the 'response' and then parse the 'response' into JSON
-//   .then(children => {               // get access to that json data
-//       console.log(children);
+  // fetch(childrenURL)
+  // .then(response => response.json()) // capture the 'response' and then parse the 'response' into JSON
+  // .then(children => {               // get access to that json data
+  //     console.log(children);
     getchildren()
+    // })
   })
 
   function getchildren() {
       fetch(childrenURL)
       .then(response => response.json())
-      .then(children => {
+      .then(children => {                 //getting my children array
         children.data.forEach(child => {
             // double check how your data is nested in the console so you can successfully access the attributes of each individual object
             const childMarkup = `
