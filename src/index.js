2e69d3e373941fd8a6c86d4745a942c141d00867
isabellagh <isabellagh88@gmail.com>
2021-06-08 13:44:05 -0400

    comments deleted

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const childrenURL = "http://localhost:3000/api/v1/children"
 document.addEventListener('DOMContentLoaded', () => {
   getChildren()
   getClassrooms()
-    // fetch and load children
-    const createClassroomForm = document.querySelector("#create-classroom-form")  //query the form  
+    
+    const createClassroomForm = document.querySelector("#create-classroom-form")    
     createClassroomForm.addEventListener("submit", (e) => classroomFormHandler(e))
     
-    const createChildForm = document.querySelector("#create-child-form")  //query the form  
+    const createChildForm = document.querySelector("#create-child-form")    
     createChildForm.addEventListener("submit", (e) => createFormHandler(e))
 })
 
@@ -19,7 +19,6 @@ document.addEventListener("click", function(e) {
     e.preventDefault()
     deleteClassroom(e.target.dataset.id)
     classroomCard.remove(classroomCard)
-    // alert("The classroom was deleted");
   }
 })
 
@@ -30,33 +29,32 @@ document.addEventListener("click", function(e) {
     e.preventDefault()
     deleteChild(e.target.dataset.id)
     childCard.remove(childCard)
-    // alert("The child was deleted");
   }
 }) 
    
-     // CLASSROOM CALLBACKS //  
+     // CLASSROOM FETCH //  
 
-function getClassrooms() {  //creating a new child
+function getClassrooms() {  
   const classroomList = document.getElementById('select-classroom')
 
   fetch(classroomsURL)
   .then(response => response.json())
-  .then(classrooms => {                 //getting my classroom array
-    classrooms.data.forEach(classroom => {  // iterate over the response ans show the data
-      // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+  .then(classrooms => {                 
+    classrooms.data.forEach(classroom => {  
+
       const newClassroom = new Classroom(classroom)  
       const classroomListItem = document.createElement('option')
 
       classroomListItem.innerHTML = classroom.attributes.room_name
       classroomListItem.value = classroom.id
       classroomList.append(classroomListItem)
-        //creates a new instance of a Classroom class. classroom object and the attributes
-        document.querySelector('#classroom-classroom-container').innerHTML += newClassroom.renderClassroomTable();
+
+      document.querySelector('#classroom-classroom-container').innerHTML += newClassroom.renderClassroomTable();
     })
   })
 }
 
-function classroomFormHandler(e) {     // handles the inputs
+function classroomFormHandler(e) {     
   e.preventDefault()
   console.log(e);            
   const roomNameInput = document.querySelector('#input-room-name').value
@@ -67,13 +65,13 @@ function classroomFormHandler(e) {     // handles the inputs
 }
 
 function classroomPostFetch(room_name, age, teacher_name, full) {
-  // build body outside of fetch
+  
   const bodyData = {classroom: {room_name, age, teacher_name, full}}  // the keys sent back (need to be the same as in the schema)
   
   fetch(classroomsURL, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(bodyData) // all attributes
+    body: JSON.stringify(bodyData) 
   })
   .then(response => response.json())
   .then(classroom => {
@@ -92,23 +90,22 @@ function deleteClassroom(id) {
 }
 
 
-        // CHILD CALLBACKS //
+        // CHILD Fetch //
 
-function getChildren() {  //creating a new child
+function getChildren() {  
   fetch(childrenURL)
   .then(response => response.json())
-  .then(children => {                 //getting my children array
-    children.data.forEach(child => {  // iterate over the response ans show the data
-        // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-      // debugger
+  .then(children => {                 
+    children.data.forEach(child => {  
+
       const newChild = new Child(child)  
-      //creates a new instance of a Child class. child object and the attributes
+      
         document.querySelector('#child-child-container').innerHTML += newChild.renderChildInfo();
       })
   })
 }
 
-function createFormHandler(e) {     // handles the inputs
+function createFormHandler(e) {     
   e.preventDefault()
   const nameInput = document.querySelector('#input-name').value
   const avatarInput = document.querySelector('#input-avatar').value
@@ -124,16 +121,16 @@ function createFormHandler(e) {     // handles the inputs
 
 function postFetch(name, avatar, age, classroom_id) {
     // build body outside of fetch
-  const bodyData = {child: {name, avatar, age, classroom_id}}  // the keys sent back (need to be the same as in the schema)  
+  const bodyData = {child: {name, avatar, age, classroom_id}}    
   fetch(childrenURL, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(bodyData) // all attributes
+    body: JSON.stringify(bodyData) 
   })
   .then(response => response.json())
   .then(child => {
     const newChild = new Child(child.data)
-    //   // render json response
+    
       document.querySelector('#child-child-container').innerHTML += newChild.renderChildInfo();
   })
 }
